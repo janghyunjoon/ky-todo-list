bc433c1237b5c4136ecb3b63a53a56e69ab8c4f3
janghyunjoon <135941433+janghyunjoon@users.noreply.github.com>
2025-08-19 11:13:46 +0900

    .

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,10 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(cors({
+    origin:process.env.FRONT_ORIGN,
+    credentials:true
+}))
 
 mongoose
     .connect(process.env.MONGO_URI)
--- frontend/src/components/TodoEditor.jsx
+++ frontend/src/components/TodoEditor.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
-import './TodoEditor.css';
+import React, { useState } from 'react'
+import "./TodoEditor.css"
+const TodoEditor = ({ onCreate }) => {
+  const [text, setText] = useState("")
 
-const TodoEditor = (params) => {
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    if (!text.trim()) return
+
+    onCreate(text.trim())
+    setText("")
+
+  }
   return (
-    <form className="Editor">
-      <input placeholder="새로운 Todo..." />
-      <button>추가</button>
+    <form className='TodoEditor' onSubmit={onSubmit}>
+      <input
+        type="text"
+        placeholder='새로운 Todo...'
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button type='submit' disabled={!text.trim()}>추가</button>
     </form>
-  );
-};
+  )
+}
 
-export default TodoEditor;
+export default TodoEditor
